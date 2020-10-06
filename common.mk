126687dbefc142770e09f2b1cec3016814697c8c
Volodymyr Zhdanov <wight554@gmail.com>
2020-10-06 19:34:43 +0300

    sm6250-common: compile drm@1.0 from source

--- common.mk
+++ common.mk
@@ -81,6 +81,9 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0 \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service \
     android.hardware.drm@1.2-service.clearkey
 
 # Fastbootd
