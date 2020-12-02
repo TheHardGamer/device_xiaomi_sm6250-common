2767813eb2f7e33c7388cda6f9887bb05c3b1678
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-12-02 13:23:08 +0530

    miatoll: Import lights HAL from CAF
    https://source.codeaurora.org/quic/la/platform/hardware/qcom/display/tree/liblight?h=LA.UM.9.1.r1-09600-SMxxx0.0
    
    Change-Id: Ie18b2c36b108c61c57801bbc42e3bfc81c12b842

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -111,6 +111,9 @@ DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):libinit_xiaomi_sm6250
 TARGET_RECOVERY_DEVICE_MODULES := libinit_xiaomi_sm6250
 
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
+
 # Media
 TARGET_USES_ION := true
 TARGET_DISABLED_UBWC := true
