04da4657e906b2956720c8017e0f07c9836c2d6c
varunhardgamer <gamelovr695@gmail.com>
2020-12-12 18:40:09 +0000

    Update vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,5 @@ rm -rf hardware/xiaomi
 git clone https://github.com/lineageos/android_hardware_xiaomi -b lineage-17.1 hardware/xiaomi
 rm -rf vendor/qcom/opensource/vibrator
 git clone https://github.com/LineageOS/android_vendor_qcom_opensource_vibrator/ -b lineage-17.1 vendor/qcom/opensource/vibrator
+rm -rf hardware/qcom-caf/sm8150/display
+git clone https://github.com/varunhardgamer/hardware_qcom_display -b lineage-17.1-caf-sm8150 hardware/qcom-caf/sm8150/display
